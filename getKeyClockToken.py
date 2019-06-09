9501abed557d3b0f6064c312ef92a6367632a054
Chaitanya Senapati <csenapati12@gmail.com>
2019-06-09 13:56:46 +0530

    Update getKeyClockToken.py

--- getKeyClockToken.py
+++ getKeyClockToken.py
@@ -1,7 +1,5 @@
 def main():
 	target_machine_ip = sys.argv[1]
- // a = int(sys.argv[1])
-	uri = "https://" + target_machine_ip + ":30443/auth/realms/hiota/protocol/openid-connect/token"
-	print(uri)
-	
+ 	uri = "https://" + target_machine_ip + ":30443/auth/realms/hiota/protocol/openid-connect/token"
+	print(uri)	
 main()
